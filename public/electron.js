d5903353af6cc29124dd0762ceff8939dd98da56
nickKBerlin <nickk69@hotmail.com>
2026-01-22 13:20:05 +0100

    Add error handling and logging to electron main process

--- public/electron.js
+++ public/electron.js
@@ -38,8 +38,15 @@ function createWindow() {
 }
 
 app.on('ready', () => {
-  db = new Database();
-  createWindow();
+  console.log('[Electron] App starting...');
+  try {
+    db = new Database();
+    console.log('[Electron] Database initialized');
+    createWindow();
+    console.log('[Electron] Window created');
+  } catch (err) {
+    console.error('[Electron] Error in app.on(ready):', err);
+  }
 });
 
 app.on('window-all-closed', () => {
@@ -55,36 +62,87 @@ app.on('activate', () => {
 });
 
 // IPC Handlers
-ipcMain.handle('get-drives', async () => {
-  return db.getDrives();
+ipcMain.handle('get-drives', async (event) => {
+  try {
+    console.log('[IPC] get-drives');
+    return await db.getDrives();
+  } catch (err) {
+    console.error('[IPC] Error in get-drives:', err);
+    throw err;
+  }
 });
 
 ipcMain.handle('add-drive', async (event, driveData) => {
-  return db.addDrive(driveData);
+  try {
+    console.log('[IPC] add-drive:', driveData.name);
+    return await db.addDrive(driveData);
+  } catch (err) {
+    console.error('[IPC] Error in add-drive:', err);
+    throw err;
+  }
 });
 
 ipcMain.handle('update-drive', async (event, driveId, driveData) => {
-  return db.updateDrive(driveId, driveData);
+  try {
+    console.log('[IPC] update-drive:', driveId);
+    return await db.updateDrive(driveId, driveData);
+  } catch (err) {
+    console.error('[IPC] Error in update-drive:', err);
+    throw err;
+  }
 });
 
 ipcMain.handle('delete-drive', async (event, driveId) => {
-  return db.deleteDrive(driveId);
+  try {
+    console.log('[IPC] delete-drive:', driveId);
+    return await db.deleteDrive(driveId);
+  } catch (err) {
+    console.error('[IPC] Error in delete-drive:', err);
+    throw err;
+  }
 });
 
 ipcMain.handle('scan-drive', async (event, driveId, drivePath) => {
-  return db.scanDrive(driveId, drivePath, (progress) => {
-    mainWindow.webContents.send('scan-progress', progress);
-  });
+  try {
+    console.log('[IPC] scan-drive:', drivePath);
+    return await db.scanDrive(driveId, drivePath, (progress) => {
+      console.log('[IPC] Scan progress:', progress.fileCount);
+      if (mainWindow) {
+        mainWindow.webContents.send('scan-progress', progress);
+      }
+    });
+  } catch (err) {
+    console.error('[IPC] Error in scan-drive:', err);
+    throw err;
+  }
 });
 
 ipcMain.handle('search-files', async (event, query, filters) => {
-  return db.searchFiles(query, filters);
+  try {
+    console.log('[IPC] search-files:', query);
+    return await db.searchFiles(query, filters);
+  } catch (err) {
+    console.error('[IPC] Error in search-files:', err);
+    throw err;
+  }
 });
 
 ipcMain.handle('get-file-stats', async (event, driveId) => {
-  return db.getFileStats(driveId);
+  try {
+    console.log('[IPC] get-file-stats:', driveId);
+    return await db.getFileStats(driveId);
+  } catch (err) {
+    console.error('[IPC] Error in get-file-stats:', err);
+    throw err;
+  }
 });
 
 ipcMain.handle('clear-drive-files', async (event, driveId) => {
-  return db.clearDriveFiles(driveId);
+  try {
+    console.log('[IPC] clear-drive-files:', driveId);
+    return await db.clearDriveFiles(driveId);
+  } catch (err) {
+    console.error('[IPC] Error in clear-drive-files:', err);
+    throw err;
+  }
 });
